83ebefce36f7184156e7ce2306d55dfc7c9cc2fd
liuye <csu1704liuye@163.com>
2022-08-18 23:41:16 +0800

    update

--- train.py
+++ train.py
@@ -292,8 +292,8 @@ def train_epoch(train_model, train_load, Device, loss_fn, eval_fn, optimizer, sc
     scheduler.step()
 
     training_dict = {
-        'loss_mean': training_loss_mean,
-        'evaluation_mean': training_eval_mean,
+        'train_loss_mean': training_loss_mean,
+        'train_evaluation_mean': training_eval_mean,
     }
     return training_loss_mean, training_eval_mean, training_dict
 
@@ -465,8 +465,8 @@ def val_epoch(valid_model, val_load, Device, loss_fn, eval_fn, epoch, Epochs, mo
         print("-" * 80)
 
     valid_dict = {
-        'loss_mean': valid_loss_mean,
-        'evaluation_mean': valid_eval_mean
+        'val_loss_mean': valid_loss_mean,
+        'val_evaluation_mean': valid_eval_mean
     }
 
     return valid_loss_mean, valid_eval_mean, valid_dict
